be3f559b604447643742042b3c2033a2eeb485f0
Nguyen Duc Dung <dungvn3000@gmail.com>
2013-02-28 02:29:37 +0700

    updated.

--- app/controllers/login/LoginController.scala
+++ app/controllers/login/LoginController.scala
@@ -1,6 +1,8 @@
 package controllers.login
 
 import play.api.mvc.{Action, Controller}
+import jp.t2v.lab.play20.auth.LoginLogout
+import auth.AuthConfigImpl
 
 /**
  * The Class LoginController.
@@ -9,10 +11,14 @@ import play.api.mvc.{Action, Controller}
  * @since 2/26/13 2:31 PM
  *
  */
-object LoginController extends Controller {
+object LoginController extends Controller with LoginLogout with AuthConfigImpl {
 
   def login = Action {
     Ok(views.html.login())
   }
+
+  def logout = Action(implicit request => {
+    gotoLogoutSucceeded
+  })
 
 }
